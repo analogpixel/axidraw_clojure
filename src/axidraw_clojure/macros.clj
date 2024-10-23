c121c1a94946e7ab745b5488725752bd0cb0c999
Matt Poepping <matt.poepping@gmail.com>
2024-10-23 11:14:34 -0600

    docs

--- src/axidraw_clojure/macros.clj
+++ src/axidraw_clojure/macros.clj
@@ -1,13 +1,16 @@
 (ns axidraw-clojure.macros)
 
-; point to vector 
+; convert point {:x :y} to vector [x y]
 (defmacro p2v [point]
   `[(:x ~point) (:y ~point)])
 
+; convert point {:x :y} to vector [x y] and scale it
 (defmacro p2v [point scale]
   `[(* (:x ~point) ~scale) (* (:y ~point) ~scale)])
 
 
+; doall is there so the plotter will actually plot 
+; otherwise clojure's laziness takes over
 (defmacro do-range [f start stop step]
   `(doall 
      (map ~f 
